b4e7046ce92ab9dfdf2e0cd8f6b41d00d8710a29
Tourgui Abderrahim <77793447+turguiabderrahim@users.noreply.github.com>
2024-05-24 02:13:07 +0100

    Update ProductCard.js

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -52,7 +52,7 @@ export default function ProductCard({product}) {
                 <div className="mt-2 mb-5 flex items-center justify-between">
                 <p>
                     <span className="text-xl font-bold text-slate-900">{product.price}</span>
-                    <span className="text-sm text-slate-900 line-through">{(product.price - (product.price * product.discount) / 100).toFixed(2)}</span>
+                    <span className="text-sm text-slate-900 line-through">{(product.price - (product.price * product.discount) / 100).toFixed(2)}$</span>
                 </p>
                 <div className="flex items-center">
                     <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
