cb816206e2c926b28751f988e826039600a4edd9
theinvesibleman <ayaturgui81@gmail.com>
2024-05-25 02:16:18 +0100

    ne

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="/" className="hover:underline">
             Tugo Store™
           </a>
           . All Rights Reserved.
